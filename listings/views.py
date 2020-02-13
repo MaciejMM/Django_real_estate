2510746f36f57423b579c9e3a9fb25551dacdab3
MaciejMM <maciej.myslinski88@gmail.com>
2020-02-13 22:16:16 +0100

    Update single listing page with dynamic DB content

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Listing
-from django.core.paginator import EmptyPage,Paginator,PageNotAnInteger
+from django.core.paginator import EmptyPage,Paginator,PageNotAnInteger 
 
 # Create your views here.
 def index(request):
@@ -19,7 +19,12 @@ def index(request):
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context={
+        'listing':listing
+    }
+    return render(request, 'listings/listing.html',context)
 
 
 def search(request):
--- realtors/admin.py
+++ realtors/admin.py
@@ -4,10 +4,11 @@ from .models import Realtor
 
 
 class RealtorAdmin(admin.ModelAdmin):
-    list_display = ('id','name','email','hire_date')
-    list_display_links = ('id','name')
+    list_display = ('id','name','email','hire_date','is_mvp')
+    list_display_links = ('id', 'name', 'email')
     search_fields = ('name',)
     list_per_page = 25
+    
     
 
 admin.site.register(Realtor, RealtorAdmin)
